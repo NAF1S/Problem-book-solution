4f446de0d14f89e522094e2bab84eccb79913717
Lokman Sharif <111578019+NAFI5O@users.noreply.github.com>
2023-02-14 10:16:04 +0600

    Create A+B.cpp

---
+++ Easy problem/A+B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define ll long long
+#define pb push_back
+#define pi acos(-1)
+#define first f
+#define second s
+#define lightSpeed() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const unsigned int M = 1000000007;
+
+int main(int argc, char const *argv[]){
+	lightSpeed();
+    ll a,b;cin>>a>>b;
+    ll ans = a+b;
+    cout<<ans;
+}
